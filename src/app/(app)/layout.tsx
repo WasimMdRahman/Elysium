bd07b3be3b3957fc4ee555619ce5efb9ce03e5f5
Wasim Rahman <rahmanwasim67@gmail.com>
2025-09-09 17:51:24 +0000

    Add the logo I provided  (_for element div_)

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -42,7 +42,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
       <Sidebar>
         <SidebarHeader>
             <div className="flex items-center gap-2">
-                
+                <Logo className="h-6 w-6" />
                 <span className="text-lg font-semibold font-headline group-data-[collapsible=icon]:hidden">Elysium</span>
             </div>
         </SidebarHeader>
