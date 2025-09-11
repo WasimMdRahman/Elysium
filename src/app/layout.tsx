2d6fef071f825512ebb6faa594e1029cd3edbf28
Wasim Rahman <rahmanwasim67@gmail.com>
2025-09-11 15:37:41 +0000

    Bro, this is not kapakana font. It's comics. I am talking about the elys

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,14 +15,6 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
-          rel="stylesheet"
-        />
-        <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Kapanaka&display=swap" rel="stylesheet" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#F0F4F8" />
       </head>
