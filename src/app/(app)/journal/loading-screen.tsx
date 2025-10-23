34669c36d4718bb4ded7be4b6379f6bf68600a73
Wasim Rahman <rahmanwasim67@gmail.com>
2025-10-23 14:14:00 +0000

    Bro, here it is for the journal.
    <!DOCTYPE html>
    <html lang="en">
    <hea

--- src/app/(app)/journal/loading-screen.tsx
+++ src/app/(app)/journal/loading-screen.tsx
@@ -4,7 +4,11 @@ import './animation.css';
 
 export const JournalLoadingScreen = () => (
     <div className="loading-screen-journal">
-        <div className="loading-emoji-journal">📔</div>
+        <div className="book">
+            <div className="cover"></div>
+            <div className="page"></div>
+            <div className="pen"></div>
+        </div>
         <div className="loading-text-journal">Loading Journal...</div>
     </div>
 );
