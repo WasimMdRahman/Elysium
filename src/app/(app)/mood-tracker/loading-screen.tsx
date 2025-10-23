e4f23d7de4c856f669ba827a02d8bb6e0a220a60
Wasim Rahman <rahmanwasim67@gmail.com>
2025-10-23 14:09:44 +0000

    Bro, I want to change the loading animation style of Mood Tracker, pleas

--- src/app/(app)/mood-tracker/loading-screen.tsx
+++ src/app/(app)/mood-tracker/loading-screen.tsx
@@ -5,6 +5,6 @@ import './animation.css';
 export const MoodTrackerLoadingScreen = () => (
     <div className="loading-screen-mood">
         <div className="loading-emoji-mood">😊</div>
-        <div className="loading-text-mood">Loading Mood Tracker...</div>
+        <div className="loading-text-mood">Loading Mood Tracker</div>
     </div>
 );
